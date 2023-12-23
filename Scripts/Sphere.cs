3c25d4fb732fbf8aa3f80aecc7857974382e2bf2
KaslGame <134434676+KaslGame@users.noreply.github.com>
2023-12-23 11:13:58 +0300

    Update Sphere.cs

--- Scripts/Sphere.cs
+++ Scripts/Sphere.cs
@@ -7,9 +7,11 @@ public class Sphere : MonoBehaviour
 {
     [SerializeField] private float _duration;
 
+    private float _xOffset = 5f;
+
     private void Start()
     {
-        Vector3 targetPosition = new Vector3(transform.position.x + 5f, transform.position.y, transform.position.z);
+        Vector3 targetPosition = new Vector3(transform.position.x + _xOffset, transform.position.y, transform.position.z);
         transform.DOMove(targetPosition, _duration).SetLoops(-1, LoopType.Yoyo);
     }
 }
