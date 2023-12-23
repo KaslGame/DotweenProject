5cf95b48a51e793f34ef9f719586ac5d1109b915
KaslGame <134434676+KaslGame@users.noreply.github.com>
2023-12-23 11:04:53 +0300

    Update Cube.cs

--- Scripts/Cube.cs
+++ Scripts/Cube.cs
@@ -8,10 +8,11 @@ public class Cube : MonoBehaviour
     [SerializeField] private float _duration;
     [SerializeField] private float _scaleChange;
     [SerializeField] private Vector3 targetRotation;
+    private float _xOffset = 2f;
 
     private void Start()
     {
-        Vector3 targetPosition = new Vector3(transform.position.x + 2f, transform.position.y, transform.position.z);
+        Vector3 targetPosition = new Vector3(transform.position.x + _xOffset, transform.position.y, transform.position.z);
         transform.DOScale(_scaleChange, _duration).SetLoops(-1, LoopType.Yoyo);
         transform.DOMove(targetPosition, _duration).SetLoops(-1, LoopType.Yoyo);
         transform.DORotate(targetRotation, _duration, RotateMode.Fast).SetLoops(-1, LoopType.Yoyo);
